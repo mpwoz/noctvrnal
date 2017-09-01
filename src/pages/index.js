be4e398501b7f4dd19a0c99acd8aca177e4dfb6a
Martin Wozniewicz <mwwoznie@amazon.com>
2017-08-31 19:35:21 -0700

    progress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,37 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import Slider from 'react-slick'
+
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+class WorkSlider extends React.Component {
+
+  render() {
+    var settings = {
+      arrows: true,
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    };
+    return (
+      <Slider {...settings}>
+        <div><img src='http://placekitten.com/g/640/480'/></div>
+        <div><img src='http://placekitten.com/g/640/480'/></div>
+        <div><img src='http://placekitten.com/g/640/480'/></div>
+      </Slider>
+    );
+  }
+}
+
 export default () => (
-    <div>
-        <h1>Immersive audio design for VR</h1>
-    </div>
+  <div>
+    <h1>Immersive audio design for VR</h1>
+    <WorkSlider/>
+  </div>
 )
 
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import styles from './about.module.scss'
-
 export default () => (
     <div>
         <h1>Work</h1>
